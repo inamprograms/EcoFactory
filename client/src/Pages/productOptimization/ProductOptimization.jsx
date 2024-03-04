bff641987cb8cdba7135635bb084b9e41b383a80
Ahmadjajja <ahmadjajja86@gmail.com>
2024-03-05 00:37:13 +0500

    feat: sidebar

--- client/src/Pages/productOptimization/ProductOptimization.jsx
+++ client/src/Pages/productOptimization/ProductOptimization.jsx
@@ -6,14 +6,15 @@ export default function ProductOptimization() {
     return (
         <div style={{ display: 'flex', height: '100vh', minHeight: '400px' }}>
             <Sidebar collapsed={collapsed} style={{ display: collapsed ? "none" : "block" }}>
-                {/* side bar content */}
+                {/* side bar content  */}
+                {/* Aleeza you have to work here */}
             </Sidebar>
             <main style={{ width: "100vw" }}>
                 <div onClick={() => setCollapsed(!collapsed)} style={{ cursor: "pointer" }}>
                     {/* <span class="big-icon" style={iconStyles}>{collapsed ? <>&#187;</>: <>&#171;</>}</span> */}
-                    <span className={`big-icon ${collapsed ? 'rotate-left' : 'rotate-right'}`} style={{...iconStyles, color: "#ccc"}}>{collapsed ? <>&#187;</>: <>&#171;</>}</span>
+                    <span className={`big-icon ${collapsed ? 'rotate-left' : 'rotate-right'}`} style={{ ...iconStyles, color: "#ccc" }}>{collapsed ? <>&#187;</> : <>&#171;</>}</span>
                 </div>
-                <div className="container-fluid bg-dark" style={{height: "100vh"}}>
+                <div className="container-fluid bg-dark" style={{ height: "100vh" }}>
                     <div className="row">
                         <div className="col">
                             {/* main content */}
