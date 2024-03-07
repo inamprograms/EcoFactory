1ceacceea9f46015d46306ad7ab5a6440d471464
Ahmadjajja <ahmadjajja86@gmail.com>
2024-03-07 06:57:29 +0500

    feat: dumy conversation with chat icons

--- client/src/Pages/productOptimization/ProductOptimization.jsx
+++ client/src/Pages/productOptimization/ProductOptimization.jsx
@@ -3,8 +3,11 @@ import { IoMdAttach } from "react-icons/io";
 import { ThreeDots } from "react-loader-spinner";
 import Spinner from '../../Components/loader/Spinner';
 import Sidebar from '../../Components/sideBar/SBar';
+import chatgptLogo from "./CHATGPT_LOGO_WHITE.svg";
 import './ProductOptimization.css'; // Import CSS file for additional styles
 import cogwheel from "./cogwheel-2.svg";
+
+
 export default function ProductOptimization() {
     const [collapsed, setCollapsed] = useState(false);
     const [loading, setLoading] = useState(false);
@@ -92,7 +95,7 @@ export default function ProductOptimization() {
 
                         {promptsArr.length == 0 && <div className="row" style={{ backgroundColor: "#e6e6e6", height: "70%" }}>
                             <div className="col d-flex flex-column justify-content-center align-items-center" style={{ margin: "0 auto", maxWidth: "800px" }}>
-                                <div><img src={cogwheel} style={{ width: '40px' }} alt="cogwheel" /></div>
+                                <div><img src={cogwheel} className='rounded circle' style={{ width: '40px' }} alt="cogwheel" /></div>
                                 <h3 className='heading3'>&nbsp; Just select the product you want to optimize,<br /> and describe below what you want to optimize...</h3>
                                 <p className='main-text'>&nbsp; &nbsp; Here we must include a clear and simple explanation about this functionality, using similar <br /> amount of texts, avoiding too short texts that provide weak information, also avoiding long text.</p>
                             </div>
@@ -112,12 +115,17 @@ export default function ProductOptimization() {
                                             {promptsArr.map((prompt, index) => (
                                                 <div key={index}>
                                                     {index % 2 == 0 ?
-                                                        <div className="row ">
+                                                        <div className="row " style={{ position: 'relative' }}>
+                                                            <img src="https://camo.githubusercontent.com/1e6de73a5a5d1800c3f18f294e4b019466d6daa7ac4ddbe713afc5e3ac062547/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f4434443033415148556d6b357863444d6574412f70726f66696c652d646973706c617970686f746f2d736872696e6b5f3430305f3430302f302f313639333430353830343034313f653d3137313532313238303026763d6265746126743d307a4b74676b73684967694439786d6e456a425a7158755731343547774e5676386f6d3958576b424f7259" className='rounded-circle' style={profileUserStyle} alt="" />
+
                                                             <div className="col-3"></div>
-                                                            <div className="col-9 shadow p-3 mb-5 bg-body-tertiary rounded">{promptsArr[index]}</div>
+                                                            <div className="col-9 shadow p-3 mb-5 bg-body-tertiary rounded " >
+                                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo sit dicta, explicabo consectetur, accusantium, voluptate repellendus quod fugiat aliquam iure sunt rerum officia aliquid iste delectus vero deleniti obcaecati similique voluptas nobis nostrum eaque! At qui possimus sequi? Dolore, ipsa?
+                                                            </div>
                                                         </div>
                                                         :
-                                                        <div className="row">
+                                                        <div className="row" style={{ position: 'relative' }}>
+                                                            <img src={chatgptLogo} className='rounded-circle' style={profileStyle} alt="" />
                                                             <div className="col-9 shadow-none p-3 mb-5 bg-body-tertiary rounded">Fair and Lovely Cream: This cream contains hydroquinone, a skin-lightening agent, along with
                                                                 parabens and artificial fragrances. It is marketed as a product to lighten skin tone and even out complexion.</div>
                                                             <div className="col-3"></div>
@@ -126,7 +134,8 @@ export default function ProductOptimization() {
                                                 </div>
                                             ))}
                                             {loading &&
-                                                <div className="row">
+                                                <div className="row" style={{ position: 'relative' }}>
+                                                    <img src={chatgptLogo} className='rounded-circle' style={profileStyle} alt="" />
                                                     <div className="col-9 shadow-none p-3 mb-5 bg-body-tertiary rounded" style={{ height: "100px" }}>
                                                         <ThreeDots
                                                             visible={true}
@@ -192,4 +201,20 @@ const iconStyles = {
 const textStyle = {
     fontFamily: '"Roboto", sans-serif',
     fontWeight: 700,
+};
+
+const profileStyle = {
+    textAlign: "justify",
+    position: 'absolute',
+    width: '6%',
+    bottom: '17px',
+    padding: '7px',
+    left: '10px',
+    backgroundColor: 'rgb(0, 118, 195)',
+};
+const profileUserStyle = {
+    position: 'absolute',
+    width: '10%',
+    bottom: '17px',
+    right: '10px',
 };
