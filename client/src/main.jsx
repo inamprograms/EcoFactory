d65c7b23f856d691c61e6ed895906729013f700e
Aleeza <malikaleeza886@gmail.com>
2024-04-17 15:00:20 -0800

    feat: chat functions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,28 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import DataContextProvider from "./Context/DataContext.jsx";
 import "./index.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <DataContextProvider>
     <React.StrictMode>
-        <App />
+
+    {/* toast container */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <ToastContainer />
+      {/* app */}
+      <App />
     </React.StrictMode>
   </DataContextProvider>
 );
